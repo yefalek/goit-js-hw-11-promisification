d81fe7ebea97328b9aaf59f6b6871cc2dfe9c46a
yefalek <yefremenkov.alek@gmail.com>
2021-07-04 14:47:58 +0300

    ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { functions } from 'lodash';
 import './sass/main.scss';
 
 const colors = [
@@ -11,4 +12,24 @@ const colors = [
   
 const randomIntegerFromInterval = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+};
+  
+const start = document.querySelector('[data-action="start"]');
+
+const stop = document.querySelector('[data-action="stop"]');
+
+let timer = null;
+
+function changeBgColor(e) {
+    timer = setInterval(() => {
+        const color = colors[randomIntegerFromInterval(0, 5)]
+        document.body.style.backgroundColor = color;
+    }, 1000)
+    start.disabled = true;
+ };
+
+start.addEventListener('click', changeBgColor);
+stop.addEventListener('click', () => {
+    clearInterval(timer);
+    start.disabled = false;
+});
