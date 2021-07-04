93e989195285ce0a8dd1f7eb28be65391a27fdc9
yefalek <yefremenkov.alek@gmail.com>
2021-07-04 16:17:19 +0300

    add tasks

--- src/index.js
+++ src/index.js
@@ -1,36 +1,3 @@
-import { functions } from 'lodash';
-import './sass/main.scss';
-
-const colors = [
-    '#FFFFFF',
-    '#2196F3',
-    '#4CAF50',
-    '#FF9800',
-    '#009688',
-    '#795548',
-];
-  
-const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-};
-  
-const start = document.querySelector('[data-action="start"]');
-
-const stop = document.querySelector('[data-action="stop"]');
-
-let timer = null;
-
-function changeBgColor(e) {
-    timer = setInterval(() => {
-        const color = colors[randomIntegerFromInterval(0, 5)]
-        document.body.style.backgroundColor = color;
-    }, 1000)
-    start.disabled = true;
- };
-
-start.addEventListener('click', changeBgColor);
-stop.addEventListener('click', () => {
-    clearInterval(timer);
-    start.disabled = false;
-    document.body.style.backgroundColor = colors[0];
-});
+import './partials/part1';
+import './partials/part2';
+import './partials/part3';
