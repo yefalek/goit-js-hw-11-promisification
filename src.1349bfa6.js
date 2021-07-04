eee2e9dde4db1452f7f7189f1b80c40fd0f12f7b
yefalek <77198181+yefalek@users.noreply.github.com>
2021-07-04 13:35:51 +0000

    Deploying to gh-pages from @ yefalek/goit-js-hw-11-promisification@93e989195285ce0a8dd1f7eb28be65391a27fdc9 🚀

---
+++ src.1349bfa6.js
@@ -0,0 +1,10 @@
+parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcelRequire,u="function"==typeof require&&require;function f(t,n){if(!r[t]){if(!e[t]){var i="function"==typeof parcelRequire&&parcelRequire;if(!n&&i)return i(t,!0);if(o)return o(t,!0);if(u&&"string"==typeof t)return u(t);var c=new Error("Cannot find module '"+t+"'");throw c.code="MODULE_NOT_FOUND",c}p.resolve=function(r){return e[t][1][r]||r},p.cache={};var l=r[t]=new f.Module(t);e[t][0].call(l.exports,p,l,l.exports,this)}return r[t].exports;function p(e){return f(p.resolve(e))}}f.isParcelRequire=!0,f.Module=function(e){this.id=e,this.bundle=f,this.exports={}},f.modules=e,f.cache=r,f.parent=o,f.register=function(r,t){e[r]=[function(e,r){r.exports=t},{}]};for(var c=0;c<t.length;c++)try{f(t[c])}catch(e){i||(i=e)}if(t.length){var l=f(t[t.length-1]);"object"==typeof exports&&"undefined"!=typeof module?module.exports=l:"function"==typeof define&&define.amd?define(function(){return l}):n&&(this[n]=l)}if(parcelRequire=f,i)throw i;return f}({"rQfx":[function(require,module,exports) {
+const e=e=>new Promise((o,t)=>{setTimeout(()=>{o(e)},e)}),o=e=>console.log(`Resolved after ${e}ms`);e(2e3).then(o),e(1e3).then(o),e(1500).then(o);
+},{}],"Oolp":[function(require,module,exports) {
+const a=[{name:"Mango",active:!0},{name:"Poly",active:!1},{name:"Ajax",active:!0},{name:"Lux",active:!1}],e=(a,e)=>Promise.all(a.map(a=>a.name===e?{...a,active:!a.active}:a)),n=a=>console.table(a);e(a,"Mango").then(n),e(a,"Lux").then(n);
+},{}],"G5Bb":[function(require,module,exports) {
+
+},{}],"Focm":[function(require,module,exports) {
+"use strict";require("./partials/part1"),require("./partials/part2"),require("./partials/part3");
+},{"./partials/part1":"rQfx","./partials/part2":"Oolp","./partials/part3":"G5Bb"}]},{},["Focm"], null)
+//# sourceMappingURL=/goit-js-hw-11-promisification/src.1349bfa6.js.map
